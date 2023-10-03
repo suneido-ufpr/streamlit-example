77084b1e4a7c123aaf30553dcd8cb89f749a6621
suneido-ufpr <146842880+suneido-ufpr@users.noreply.github.com>
2023-10-03 14:49:53 -0300

    menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,6 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
