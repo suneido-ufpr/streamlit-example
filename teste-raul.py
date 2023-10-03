379c6041fbc233941f8a9d076a5ba0c2f1606dce
suneido-ufpr <146842880+suneido-ufpr@users.noreply.github.com>
2023-10-03 10:11:37 -0300

    Update teste-raul.py

--- teste-raul.py
+++ teste-raul.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
+# Adicionar conteúdo à página Teste Raul
+if st.session_state.page_name == "teste-raul":
+    # Colocar o código da sua página Teste Raul aqui
+    st.write("Hello World!")
+  
 # Exibir a mensagem "Hello World!"
 st.write("Hello World!")
