9f2938d1eedb469fea6682cdb20f08448e91e51c
suneido-ufpr <146842880+suneido-ufpr@users.noreply.github.com>
2023-10-03 15:21:27 -0300

    menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.set_page_config(
     }
 )
 
-st.help(st.sidebar.option_menu)
+st.help(st.sidebar)
 
 #st.sidebar.selectbox("streamlit app", on_click=st.warning('This is a warning', icon="⚠️"), args=["home"])
 
