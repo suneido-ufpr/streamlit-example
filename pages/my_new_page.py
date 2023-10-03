14b6f370075c6c8bec7e629d3c2f8fa491dcef19
suneido-ufpr <146842880+suneido-ufpr@users.noreply.github.com>
2023-10-03 13:52:07 -0300

    Update my_new_page.py

--- pages/my_new_page.py
+++ pages/my_new_page.py
@@ -4,7 +4,7 @@ def my_new_page():
     st.write("This is my new page!")
 
 # st.sidebar.markdown("# Pages")
-# st.sidebar.button("Home", on_click=st.session_state.set, args=["home"])
+st.sidebar.button("streamlit app", on_click=st.session_state.set, args=["home"])
 # st.sidebar.button("My new page", on_click=st.session_state.set, args=["my_new_page"])
 
 if st.session_state.get("page") == "my_new_page":
