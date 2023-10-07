95afe643f85c8eb63f06eaf7dc6adfedd68fc153
suneido-ufpr <146842880+suneido-ufpr@users.noreply.github.com>
2023-10-07 16:14:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,113 @@
+#Simplesmente executando essa função você irá obter um novo arquivo CSV com os dados aleatórios. Agora, para exibir a tabela basta utilizar a biblioteca Streamlit.
+
+#Execute essa linha após a execução da função generate_data(). Ela irá carregar o novo arquivo CSV e exibi-lo como uma tabela.
+#Agora você tem um app completo para realizar todas as etapas solicitadas pelo usuário.
+
+
 import streamlit as st
+
+import pandas as pd
+
+from faker import Faker
+
+fake = Faker()
+import random
+import string
+import re
+import numpy as np
+import math
+import time
+import csv
+
+#Função para ler o arquivo CSV
+def read_file(filename):
+
+df = pd.read_csv(filename)
+
+return df
+#Função para gerar dados aleatórios baseados nos padrões encontrados
+def generate_data():
+
+# Leitura do arquivo CSV
+
+data = read_file("RefundProcess.csv")
+
+# Identificação dos padrões presentes nas colunas
+status_patterns = []
+employee_patterns = []
+complete_timestamp_patterns = []
+channel_patterns = []
+
+for column in data.columns:
+    if column == 'Order Nr.':
+        continue
+
+    pattern = r''.join([random.choice(['[A-Z]'] + [random.choice(string.digits)] * (len(column)-1)) for _ in range(10)])
+    regexp = re.compile('^'+pattern+'$')
+    matches = list(filter(regexp.match, data['Order Nr.'].unique()))
+    status_patterns.append((column, pattern, len(matches)))
+    pattern = r'\w+'
+    regexp = re.compile('^'+pattern+'$')
+    matches = list(set(list(filter(regexp.match, data[column].unique())).difference({np.nan})))
+    employee_patterns.append((column, pattern, len(matches), matches))
+
+    pattern = r'\d{2}/\d{2}/\d{2}\s+\d{2}:\d{2}'
+    regexp = re.compile('^'+pattern+'$')
+    matches = list(set(list(filter(regexp.match, data[column].unique())).difference({np.nan}))[:math.ceil(len(data)/10)])
+    complete_timestamp_patterns.append((column, pattern, len(matches), matches))
+
+    pattern = r'\w+'
+    regexp = re.compile('^'+pattern+'$')
+    matches = list(set(list(filter(regexp.match, data[column].unique())).difference({np.nan}))[:math.ceil(len(data)/10)])
+    channel_patterns.append((column, pattern, len(matches), matches))
+
+print(status_patterns)
+print(employee_patterns)
+print(complete_timestamp_patterns)
+print(channel_patterns)
+
+# Geração de dados aleatórios
+new_rows = []
+for i in range(1000):
+    row = {}
+    for column, pattern, count, choices in status_patterns:
+        row[column] = fake.lexify(text=pattern)
+
+    for column, pattern, count, choices in employee_patterns:
+        row[column] = random.sample(choices, k=1)[0]
+
+    for column, pattern, count, choices in complete_timestamp_patterns:
+        row[column] = fake.date("%m/%d/%Y %H:%M", pattern="%m/%d/%y %H:%M")
+
+    for column, pattern, count, choices in channel_patterns:
+        row[column] = random.sample(choices, k=1)[0]
+
+    new_rows.append(row)
+
+# Criação de um DataFrame com os dados aleatórios
+new_df = pd.DataFrame(new_rows)
+
+# Adição dos dados ao dataframe original
+result = pd.concat([data, new_df], ignore_index=True)
+
+# Escrita do novo dataframe em um arquivo CSV
+timestamp = int(time.time())
+filename = f"RefundProcess_{str(timestamp)}.csv"
+result.to_csv(filename, index=False)
+
+return filename
+
+#Executando a função
+generate_data()
+
+st.table(pd.read_csv("RefundProcess_" + str(timestamp) + ".csv"))
+
+
+'''import streamlit as st
 
 st.header("Main Page")
 st.slider("x")
-
+'''
 
 
 '''
